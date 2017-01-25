4c09cffdd1ae4a03e25f373ed800fe71a8014e3b
vbarot1 <vbarot1@student.gsu.edu>
2017-01-25 17:40:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dinesh/viraldemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.dinesh.viraldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton bt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialView();
+        initialListener();
+    }
+
+    private void initialView() {
+
+        bt1 = (ImageButton) findViewById(R.id.bt1);
+
+    }
+
+    private void initialListener(){
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"Button2 was clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    public void onClick(View view) {
+        Toast.makeText(this,"Button1 was clicked",Toast.LENGTH_LONG).show();
+    }
+}
